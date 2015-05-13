ca6a3ef811b458f31f3c2304c7d0e9235d460bac
vegardb <vegard.bones@met.no>
2015-05-13 11:04:30 +0200

    added explicit return type to method

--- app/controllers/template/TemplateController.scala
+++ app/controllers/template/TemplateController.scala
@@ -1,3 +1,28 @@
+/*
+    MET-API
+
+    Copyright (C) 2014 met.no
+    Contact information:
+    Norwegian Meteorological Institute
+    Box 43 Blindern
+    0313 OSLO
+    NORWAY
+    E-mail: met-api@met.no
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+    MA 02110-1301, USA
+*/
+
 package controllers.template
 
 /**
@@ -11,7 +36,7 @@ import play.api.mvc._
 
 object TemplateController extends Controller {
 
-  def sample = Action {
+  def sample: Action[AnyContent] = Action {
     Ok("Implement me")
   }
 
